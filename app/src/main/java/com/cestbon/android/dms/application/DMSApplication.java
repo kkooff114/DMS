c31fc9412e1b2bbbcccf50bfc6e0c66e5dd6d91c
kkooff114 <kkooff114@gmail.com>
2017-08-02 19:19:00 +0800

    init

---
+++ app/src/main/java/com/cestbon/android/dms/application/DMSApplication.java
@@ -0,0 +1,10 @@
+package com.cestbon.android.dms.application;
+
+import com.jess.arms.base.GodApplication;
+
+/**
+ * Created by LJW on 2017/8/2.
+ */
+
+public class DMSApplication extends GodApplication {
+}
---
+++ common/src/main/java/com/jess/arms/http/fastjson/FastJSONResponseBodyConverter.java
@@ -0,0 +1,34 @@
+package com.jess.arms.http.fastjson;
+
+import com.alibaba.fastjson.JSON;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+
+import okhttp3.ResponseBody;
+import retrofit2.Converter;
+
+/**
+ * Created by LJW on 15/10/20.
+ */
+public class FastJSONResponseBodyConverter<T> implements Converter<ResponseBody, T> {
+
+    private final Type type;
+
+    FastJSONResponseBodyConverter(Type type) {
+        this.type = type;
+    }
+
+
+    @Override
+    public T convert(ResponseBody value) throws IOException {
+        String reader = value.string();
+        try {
+            return JSON.parseObject(reader, type);
+        } catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+}
